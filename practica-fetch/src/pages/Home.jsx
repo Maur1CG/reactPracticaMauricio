978c20c16efa45b5ac62b9e911c41ef1f98aa88b
Maur1CG <20240746@ricaldone.edu.sv>
2026-04-20 09:02:14 -0600

    segundo cambio

---
+++ practica-fetch/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home (){
+    return(
+        <main className="container mx-auto flex h-screen flex-col items-center justify-center p-4 wfull">
+
+            <h1 className="text-center"> Home</h1>
+            <p>Proyecto consumiendo API´s externas</p>
+        </main>
+    );
+}
+
+export default Home;
